b5e0512edf5f68aa2f48bdca8e2e33a60940b596
Eneko Retolaza <73554201+enekoreto@users.noreply.github.com>
2025-02-14 19:30:31 +0100

    -UsersController added

---
+++ ApiFilms/Controllers/UsersController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiFilms.Controllers
+{
+    [Route("api/users")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+    }
+}
